22af63ceea9e000885aa87b65bd72fa8a35e91f0
Ben Tanaka <BenT07891@gmail.com>
2023-01-04 17:44:13 -0600

    Time: 105 ms (14.34%), Space: 42.6 MB (6.27%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -8,9 +8,17 @@ var generate = function(numRows) {
     for (let i = 1; i < numRows; i ++) {
         let subArray = new Array(i + 1);
         
-        for (let k = 0; k < subArray.length; k ++) {
-            subArray[k] = (results[i - 1][k - 1] || 0) + (results[i - 1][k] || 0);
+        for (let k = 0; k <= Math.floor(i/2); k++) {
+            let tempresult = (results[i - 1][k - 1] || 0) + (results[i - 1][k] || 0);
+            subArray[k] = tempresult;
+            subArray[i - k] = tempresult;
+            
         }
+        
+        // for (let k = 0; k < subArray.length; k ++) {
+        //     subArray[k] = (results[i - 1][k - 1] || 0) + (results[i - 1][k] || 0);
+        // }
+        
         results[i] = subArray
     }
     return results;
