e4ea04b494d26e81f6671315092dd3f402d896fe
Ben Tanaka <BenT07891@gmail.com>
2023-01-04 23:32:11 -0600

    Time: 99 ms (15.43%), Space: 42 MB (42.94%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+
+    for (let i = 1; i < nums.length; i ++) {
+        nums[i] = Math.max(nums[i] + (nums[i - 2] || 0), nums[i-1])
+    }
+    return nums[nums.length-1]
+};
