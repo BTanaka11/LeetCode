f33ffab321b59688a35c2e064861951f3ff3aaf2
Ben Tanaka <BenT07891@gmail.com>
2023-01-23 18:27:01 -0600

    Time: 212 ms (57.36%), Space: 47.9 MB (48.38%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -4,22 +4,14 @@
  */
 var numSquares = function(n) {
     
-    let resultsDict = {1: 1, 0:0};
-        
-    let recurser = (n) => {
-        
-        let historyResults = resultsDict[n];
-        if (historyResults != undefined) {
-            return historyResults;
-        }
-        let results = [];
-        for (let i = Math.floor(Math.sqrt(n)); i > 0; i --) {
-            results.push(1 + recurser(n - i*i));
-        }
-        let minResults = Math.min(...results)
-        resultsDict[n] = minResults;
-        return minResults;
-    }
-    
-    return recurser(n)
+    const dp = [0];
+  
+      for (let i = 1; i <= n; i++) {
+        dp[i] = Infinity;
+        for (let j = 1; j*j <= i; j++) {
+          dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+        } 
+      }
+
+      return dp[n];
 };
