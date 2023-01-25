0230a07b40982a0af8a5834e4b8c30b515cb00c1
Ben Tanaka <BenT07891@gmail.com>
2023-01-24 22:29:24 -0600

    Time: 67 ms (59.73%), Space: 41.9 MB (54.55%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -14,18 +14,19 @@ var decodeString = function(s) {
     while (i < s.length) {
         if (isNaN(s[i]) === false) {
             let numberStart = i;
+            i ++
             while (isNaN(s[i]) === false) {
                 i ++;
             }
             let numberEnd = i;
             let numberz = Number(s.slice(numberStart, numberEnd));
-            stackz.push('[');
-            while (stackz.length > 0) {
+            stackSize = 1;
+            while (stackSize > 0) {
                 i ++;
                 if (s[i] === '[') {
-                    stackz.push('[');
+                    stackSize ++;
                 } else if (s[i] === ']') {
-                    stackz.pop();
+                    stackSize --;
                 }
             }
             resultString += decodeString(s.slice(numberEnd + 1, i)).repeat(numberz);
