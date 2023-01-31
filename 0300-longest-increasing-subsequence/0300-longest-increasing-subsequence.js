4c3ce1da52a93b8129e8890b7ed6dcf5c4f767d8
Ben Tanaka <BenT07891@gmail.com>
2023-01-31 00:25:43 -0600

    Time: 63 ms (98.60%), Space: 42.6 MB (95.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -28,28 +28,49 @@ var lengthOfLIS = function(nums) {
 //         }
 //     }
 //     return maxSeq;
-    const binarySearchPosition = (dp, target, hi) => {
-        let lo = 0;
-        while (lo <= hi) {
-            let mid = Math.floor((lo+hi)/2);
-            if (target === dp[mid]) return mid;
-            else if (target < dp[mid]) hi = mid-1;
-            else lo = mid+1;
+    
+    var n = nums.length;
+    if (!n) return 0;
+    var len = 1;
+    var dp = [nums[0]];
+    for (var i = 1; i < n; i++) {
+        if (dp[len - 1] < nums[i]) {
+            dp[len++] = nums[i];
+        } else {
+            var left = 0, right = len - 1, num = nums[i];
+            while (left < right) {
+                var mid = left + Math.floor((right - left) / 2);
+                if (dp[mid] < num) left = mid + 1;
+                else right = mid;
+            }
+            dp[right] = num;
         }
-        return lo;
     }
+    return len;
     
-    if (nums === null || nums.length===0) return 0;
-    if (nums.length === 1) return 1;
-    let dp = new Array(nums.length).fill(Number.MAX_SAFE_INTEGER);
-    for (let i=0; i<nums.length; i++){
-        let pos = binarySearchPosition(dp, nums[i], i);
-        dp[pos] = nums[i];
-    }
+    
+//     const binarySearchPosition = (dp, target, hi) => {
+//         let lo = 0;
+//         while (lo <= hi) {
+//             let mid = Math.floor((lo+hi)/2);
+//             if (target === dp[mid]) return mid;
+//             else if (target < dp[mid]) hi = mid-1;
+//             else lo = mid+1;
+//         }
+//         return lo;
+//     }
+    
+//     if (nums === null || nums.length===0) return 0;
+//     if (nums.length === 1) return 1;
+//     let dp = new Array(nums.length).fill(Number.MAX_SAFE_INTEGER);
+//     for (let i=0; i<nums.length; i++){
+//         let pos = binarySearchPosition(dp, nums[i], i);
+//         dp[pos] = nums[i];
+//     }
 
-    for (let i = dp.length-1; i >= 0; i--){
-        if (dp[i] !== Number.MAX_SAFE_INTEGER) return i+1;
-    }
+//     for (let i = dp.length-1; i >= 0; i--){
+//         if (dp[i] !== Number.MAX_SAFE_INTEGER) return i+1;
+//     }
     
-    return 0;
+//     return 0;
 };
