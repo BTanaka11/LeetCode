266aab71cfb9b4ee9bf1acb618a569904aabca69
Ben Tanaka <BenT07891@gmail.com>
2023-01-31 20:44:53 -0600

    Time: 77 ms (65.63%), Space: 44.3 MB (61.10%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
@@ -12,28 +12,43 @@
  */
 var flatten = function(root) {
 
-    if (!root) {
-        return null;
-    }
+//     if (!root) {
+//         return null;
+//     }
     
-    if (root.left === null && root.right === null) {
-        return root;
-    }
+//     if (root.left === null && root.right === null) {
+//         return root;
+//     }
     
-    let leftTail = root.left ? flatten(root.left) : null;
-    let rightTail = root.right ? flatten(root.right) : null;
+//     let leftTail = root.left ? flatten(root.left) : null;
+//     let rightTail = root.right ? flatten(root.right) : null;
     
-    if (leftTail && rightTail) {
-        leftTail.right = root.right;
+//     if (leftTail && rightTail) {
+//         leftTail.right = root.right;
+//         root.right = root.left;
+//         root.left = null;
+//         return rightTail;
+//     } else if (leftTail && !rightTail) {
+//         root.right = root.left;
+//         root.left = null;
+//         return leftTail;
+//     } else if (!leftTail && rightTail) {
+//         return rightTail;
+//     }
+    if (root === null) return;
+    if (root.left) {
+		// step 1
+        var last = root.left;
+        while (last.right !== null) last = last.right;
+        // step 2
+        var tmp = root.right;
+		// step 3
         root.right = root.left;
+		// step 4
+        last.right = tmp;
+		// step 5
         root.left = null;
-        return rightTail;
-    } else if (leftTail && !rightTail) {
-        root.right = root.left;
-        root.left = null;
-        return leftTail;
-    } else if (!leftTail && rightTail) {
-        return rightTail;
     }
     
+    flatten(root.right);
 };
