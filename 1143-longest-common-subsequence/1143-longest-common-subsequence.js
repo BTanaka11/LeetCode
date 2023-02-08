e15cfe2db555eec33b08a1628cf8ad5d7aa2eac9
Ben Tanaka <BenT07891@gmail.com>
2023-02-07 23:41:17 -0600

    Time: 160 ms (25.44%), Space: 64.4 MB (22.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -5,6 +5,28 @@
  */
 var longestCommonSubsequence = function(text1, text2) {
     
+    let hashText1 = {};
+    text1.split('').forEach((letter)=> {
+        hashText1[letter]=1;
+    });
+    
+    let hashText2 = {};
+    let newText2 = []
+    text2.split('').forEach((letter)=> {
+        if (hashText1[letter]) {
+            hashText2[letter]=1;
+            newText2.push(letter)
+        }
+    });
+    let newText1 = [];
+    text1.split('').forEach((letter)=> {
+        if (hashText2[letter]) {
+            newText1.push(letter);
+        }
+    });
+    text1 = newText1;
+    text2 = newText2;
+    
     let nestedArray = new Array(text1.length + 1).fill(null);
     nestedArray = nestedArray.map(()=> (new Array(text2.length + 1).fill(null).map(()=>0)))
 
