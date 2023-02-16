520c9d7161b92554d0a7d0a59e6e01f75c88656e
Ben Tanaka <BenT07891@gmail.com>
2023-02-15 21:29:13 -0600

    Time: 62 ms (69.39%), Space: 41.5 MB (87.87%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,44 +3,33 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    let results = [];
-    let topRowIndex = 0;
-    let bottomRowIndex = matrix.length - 1;
-    let leftColIndex = 0;
-    let rightColIndex = matrix[0].length - 1;
-    let typz = 1;
-    while (results.length < (matrix.length * matrix[0].length)) {
-        switch (typz) {
-            case 1:
-                for (let c = leftColIndex; c <= rightColIndex; c ++) {
-                    results.push(matrix[topRowIndex][c]);
-                }
-                topRowIndex ++;
-                break;
-            case 2:
-                for (let r = topRowIndex; r <= bottomRowIndex; r ++) {
-                    results.push(matrix[r][rightColIndex])
-                };
-                rightColIndex --;
-                break;
-            case 3:
-                for (let c = rightColIndex; c >= leftColIndex; c --) {
-                    results.push(matrix[bottomRowIndex][c]);
-                }
-                bottomRowIndex--;
-                break;
-            case 4:
-                for (let r = bottomRowIndex; r >= topRowIndex; r --) {
-                    results.push(matrix[r][leftColIndex]);
-                };
-                leftColIndex ++;
-                break;
+    let results = []
+    let rowProcessed = 0;
+    let colProcessed = 0;
+    while (results.length < matrix.length * matrix[0].length) {
+        for (let c = colProcessed; c < matrix[0].length - colProcessed; c++) {
+            results.push(matrix[rowProcessed][c]);
         }
-        if (typz === 4) {
-            typz = 1;
-        } else {
-            typz++;
+        rowProcessed++;
+
+        for (let r = rowProcessed; r <= matrix.length - rowProcessed; r ++) {
+            results.push(matrix[r][matrix[0].length - 1 - colProcessed]);
+        }
+        colProcessed ++;
+
+        if (rowProcessed <= matrix.length/2) {
+            for (let c = matrix[0].length - 1 - colProcessed; c >= colProcessed -1; c --) {
+                results.push(matrix[matrix.length - rowProcessed][c]);
+            }
+        }
+
+        if (colProcessed <= matrix[0].length/2) {
+            for (let r = matrix.length-1 - rowProcessed; r >= rowProcessed; r --) {
+                results.push(matrix[r][colProcessed - 1]);
+            }
         }
     }
-    return results;
+    return results
+    
 };
+//[[1,2,3,4,5,6,7,8,9,10],[11,12,18,28,38,48,57,67,77,87],[96,99,98,97,15,25,35,45,54,64],[74,84,91,92,93,94,12,22,23,24]]
