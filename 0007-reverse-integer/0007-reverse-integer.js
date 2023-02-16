ed5e26ce468a6f1f55c910544757b04187046407
Ben Tanaka <BenT07891@gmail.com>
2023-02-16 01:15:48 -0600

    Time: 72 ms (82.37%), Space: 43.2 MB (83.50%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,21 +3,13 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let result = 0;
-    let neg;
-    if (x < 0) {
-        neg = -1;
-        x = Math.abs(x)
-    } else {
-        neg = 1;
-    }
+    let rev = 0;
 
     while (x != 0) {
-        let firstDig = x % 10;
-        result += (x % 10) * (10 ** Math.floor(Math.log10(x)));
-        x = Math.floor(x / 10);
+        rev = rev * 10 + x % 10
+        x = ~~(x / 10);
     }
     
-    return (result < ((-2)**31) || result > (2**31 - 1)) ? 0 : result * neg;
+    return (rev < ((-2)**31) || rev > (2**31 - 1)) ? 0 : rev;
     
 };
