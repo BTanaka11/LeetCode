01a35141450c6cca88a7a3f8151872515c3ea34c
Ben Tanaka <BenT07891@gmail.com>
2023-02-17 23:44:09 -0600

    Time: 75 ms (49.43%), Space: 44.6 MB (37.29%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    if (n === 1) {
+        return "1";
+    }
+    let result = countAndSay(n - 1);
+    let count = 1;
+    let resultStr = [];
+    for (let i = 0; i < result.length; i ++) {
+        if (result[i + 1] != result[i]) {
+            resultStr.push(count, result[i])
+            count = 1;
+        } else {
+            count ++
+        }
+    }
+    return resultStr.join('');
+};
