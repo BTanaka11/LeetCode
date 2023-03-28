b57568a02654162fce870420557e389e1c392441
Ben Tanaka <BenT07891@gmail.com>
2023-03-27 23:15:55 -0500

    Time: 508 ms (52.03%), Space: 82.7 MB (50.72%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+
+    let exclusionArray = new Array(n).fill(null);
+    let result = 0;
+    for (let i = 2; i < n; i ++) {
+        if (!exclusionArray[i]) {
+            result ++;
+            for (let j = i * 2; j < n; j += i) {
+                exclusionArray[j] = 1;
+            }
+        }
+    }
+    return result;
+};
+
